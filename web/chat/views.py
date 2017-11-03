9dedd0402043b509d1cf463424ae86ba03bc5b5c
Jonathan Peterson <JPeterson462@gmail.com>
2017-11-02 23:56:06 -0400

    Finished server

--- web/chat/views.py
+++ web/chat/views.py
@@ -1,9 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse
+import json
+import datetime
 
+from django.db import connection
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Message
+
 def index(request):
-	return  HttpResponse("Hello world")
+	if request.method == 'GET':
+		messages = Message.objects.order_by('msg_date').all()
+		response_data = []
+		final_response = {}
+		for m in messages:
+			response_record = {}
+			response_record['user'] = m.user
+			response_record['date'] = m.msg_date.strftime("%Y-%m-%d_%H-%M-%S")
+			response_record['text'] = m.chat_text
+			response_data.append(response_record)
+		final_response["log"] = response_data
+		return HttpResponse(json.dumps(final_response, sort_keys=True), "application/json")
+	else:
+		return HttpResponse("")
+
+def create(request):
+	dateStr = request.GET['date']
+	date = datetime.datetime.strptime(dateStr, "%Y-%m-%d_%H-%M-%S")
+	m = Message(user=request.GET['user'], msg_date=date, chat_text=request.GET['text'])
+	m.save()
+	return HttpResponse("Success!")
